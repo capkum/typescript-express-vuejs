3d80a8239543ab7ff2ccb111e5f8d8b2173beb03
Seoungjin Kim <capnaver@gmail.com>
2018-10-04 08:41:24 +0900

    vue 샘플코드 작성

---
+++ src/assets/js/common/vueConfig.js
@@ -0,0 +1,8 @@
+Vue.options.delimiters = ['{$', '$}'];
+
+const veeConf = {
+  locale: 'ko',
+}
+
+// axios.defaults.headers.common['X-CSRFToken'] = "{% csrf_token %}"
+// axios.defaults.xsrfHeaderName = 'X-CSRFToken';
